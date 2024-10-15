2c75c811213f486717877ea5202194c858dca5e3
Robert <94055223+RobRussellManc@users.noreply.github.com>
2024-10-15 15:06:00 +0100

    Add delete button and read buttons

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const myLibrary = [
     ['The Hobbit', 'J.R.R. Tolkien', '295', 'yes'],
-    ['The Test', 'J.TEEEEST.R. Tolkien', 'ssd295', 'no']
+    ['The Test', 'J.TEEEEST.R. Tolkien', '425', 'no']
 ];
 
 console.table(myLibrary);
@@ -60,16 +60,29 @@ function addBookToLibrary() {
     addBookToPage(myLibrary);
 }
 
+
+function updateReadStatus(i, bookRead, buttons) {
+    // Update the read status's in MyLibrary
+    if (bookRead == 'yes') {
+        myLibrary[i][3] = 'yes';
+        
+    } else {
+        myLibrary[i][3] = 'no';
+        
+    }
+    
+    console.table(myLibrary);
+    addBookToPage(myLibrary);
+}
+
+
 function addBookToPage(bookArray) {
 
     // Select the container displaying book cards
     const book_container = document.querySelector(".main_box");
 
     // Clear the container
-    book_container.innerHTML = '';
-    
-
-    
+    book_container.innerHTML = '';    
 
     for (let i = 0; i < bookArray.length; i++) {
         let currentBook = bookArray[i]
@@ -77,6 +90,20 @@ function addBookToPage(bookArray) {
         // Create bookbox div
         const newDiv = document.createElement("div");
         newDiv.className = 'book_box';
+
+        // Create spans/headers for each bit of info
+        var bookNameHeader = document.createElement("span");
+        bookNameHeader.innerText = 'Book title';
+        bookNameHeader.classList.add('bookHeader');
+        var authorNameHeader = document.createElement("span");
+        authorNameHeader.innerText = 'Author';
+        authorNameHeader.classList.add('bookHeader');
+        var pagesHeader = document.createElement("span");
+        pagesHeader.innerText = '# pages';
+        pagesHeader.classList.add('bookHeader');
+        var hasReadHeader = document.createElement("span");
+        hasReadHeader.innerText = 'Book completed?';
+        hasReadHeader.classList.add('bookHeader');
 
         // Add book name
         var bookName = document.createElement("span")
@@ -91,25 +118,53 @@ function addBookToPage(bookArray) {
         // Add number pages
         var numberPages = document.createElement("span")
         numberPages.id = 'pages';
-        numberPages.innerText = currentBook[2];
+        numberPages.innerText = currentBook[2] + ' pages';
 
         // Add has read
         var hasRead = document.createElement("span")
-        hasRead.id = 'has_read';
-        hasRead.innerText = currentBook[3];
+        hasRead.id = 'hasRead';
+        hasRead.innerHTML = 'Book completed?<br>';
 
-        
-        
+        // Create read/not read buttons
+        var yesRead = document.createElement("button");
+        yesRead.innerText = 'Yes';
+        yesRead.classList.add('readButton');
+        yesRead.addEventListener("click", () => {updateReadStatus(i, 'yes')});
+
+
+        var noRead = document.createElement("button");
+        noRead.innerText = 'No'
+        noRead.classList.add('readButton');
+        noRead.addEventListener("click", () => {updateReadStatus(i, 'no')});
+
+        // Set the default colors of the read buttons
+        if (currentBook[3] == 'yes') {
+            yesRead.classList.add('yesReadButton');
+        } else {
+            noRead.classList.add('noReadButton');
+        }
+
+        hasRead.appendChild(yesRead);
+        hasRead.appendChild(noRead);        
         
         // Add delete button
         var delButton = document.createElement("button")
         delButton.id = 'delButton';
         delButton.value = i;
-        delButton.innerHTML = 'Delete ' + i;
+        delButton.innerHTML = 'Delete';
         delButton.addEventListener("click", () => {deletePressed(i)})
         
+
+        // Create spans/headers for each bit of info
+        var textby = document.createElement("span");
+        textby.innerText = 'by';
+        textby.classList.add('bookHeader');
+
+     
         // Append each span to the book box
         newDiv.appendChild(bookName);
+        // Add the word 'by' between bookname and author
+        newDiv.appendChild(textby);
         newDiv.appendChild(author);
         newDiv.appendChild(numberPages);
         newDiv.appendChild(hasRead);
