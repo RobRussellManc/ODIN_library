a2101af1afeefb4f959d7a9f007e9979412b2474
Robert <94055223+RobRussellManc@users.noreply.github.com>
2024-10-15 18:08:51 +0100

    Updated to use an array of classes

--- javascript.js
+++ javascript.js
@@ -1,10 +1,26 @@
 const myLibrary = [
-    ['The Hobbit', 'J.R.R. Tolkien', '295', 'yes'],
-    ['The Test', 'J.TEEEEST.R. Tolkien', '425', 'no']
+
 ];
 
+function Book(title, author, pages, has_read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.has_read = has_read;
+}
+
+
+const starting_books =  [['The Hobbit', 'J.R.R. Tolkien', '295', 'yes'],
+['The Test', 'J.TEEEEST.R. Tolkien', '425', 'no']];
+
+for (let i = 0; i < 2; i++) {
+    let book = new Book(...starting_books[i]);
+    myLibrary.push(book);
+}
+
+
 console.table(myLibrary);
-//console.log(myLibrary.length)
+
 
 
 // On page load, add all books in library
@@ -53,9 +69,9 @@ function addBookToLibrary() {
     console.log(bookName);
     form.reset();
     
-
-    const newBookArray = [bookName, authorName, numPages, hasRead]
-    myLibrary.push(newBookArray);
+    let book = new Book(bookName, authorName, numPages, hasRead )
+    //const newBookArray = [bookName, authorName, numPages, hasRead]
+    myLibrary.push(book);
 
     addBookToPage(myLibrary);
 }
@@ -64,10 +80,10 @@ function addBookToLibrary() {
 function updateReadStatus(i, bookRead, buttons) {
     // Update the read status's in MyLibrary
     if (bookRead == 'yes') {
-        myLibrary[i][3] = 'yes';
+        myLibrary[i].has_read = 'yes';
         
     } else {
-        myLibrary[i][3] = 'no';
+        myLibrary[i].has_read  = 'no';
         
     }
     
@@ -91,34 +107,22 @@ function addBookToPage(bookArray) {
         const newDiv = document.createElement("div");
         newDiv.className = 'book_box';
 
-        // Create spans/headers for each bit of info
-        var bookNameHeader = document.createElement("span");
-        bookNameHeader.innerText = 'Book title';
-        bookNameHeader.classList.add('bookHeader');
-        var authorNameHeader = document.createElement("span");
-        authorNameHeader.innerText = 'Author';
-        authorNameHeader.classList.add('bookHeader');
-        var pagesHeader = document.createElement("span");
-        pagesHeader.innerText = '# pages';
-        pagesHeader.classList.add('bookHeader');
-        var hasReadHeader = document.createElement("span");
-        hasReadHeader.innerText = 'Book completed?';
-        hasReadHeader.classList.add('bookHeader');
+ 
 
         // Add book name
         var bookName = document.createElement("span")
         bookName.id = 'bookName';
-        bookName.innerText = currentBook[0];
+        bookName.innerText = currentBook.title;
 
         // Add author
         var author = document.createElement("span")
         author.id = 'author';
-        author.innerText = currentBook[1];
+        author.innerText = currentBook.author;
 
         // Add number pages
         var numberPages = document.createElement("span")
         numberPages.id = 'pages';
-        numberPages.innerText = currentBook[2] + ' pages';
+        numberPages.innerText = currentBook.pages + ' pages';
 
         // Add has read
         var hasRead = document.createElement("span")
@@ -138,7 +142,7 @@ function addBookToPage(bookArray) {
         noRead.addEventListener("click", () => {updateReadStatus(i, 'no')});
 
         // Set the default colors of the read buttons
-        if (currentBook[3] == 'yes') {
+        if (currentBook.has_read == 'yes') {
             yesRead.classList.add('yesReadButton');
         } else {
             noRead.classList.add('noReadButton');
